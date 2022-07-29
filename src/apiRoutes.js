04d251849fec49a518fa33f47355602cf9ecf2f4
backtofayton <yucelolcay@gmail.com>
2022-07-29 11:47:14 +0300

    Update CORS

--- src/apiRoutes.js
+++ src/apiRoutes.js
@@ -1,9 +1,9 @@
-const PROXY = 'https://safe-ridge-22036.herokuapp.com';
+// const PROXY = 'https://safe-ridge-22036.herokuapp.com';
 export const REMOTE_BASE = 'https://products-listing-demo.herokuapp.com';
 
 const base =
   process.env.NODE_ENV === 'production'
-    ? `${PROXY}/${REMOTE_BASE}/index.php/api`
+    ? `${REMOTE_BASE}/index.php/api`
     : '/api';
 
 export default {
